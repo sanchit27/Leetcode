bdde1ce5aa461daaf75e031034f08383f980ef78
Sanchit Bhargava <saanchit@gmail.com>
2022-09-15 20:01:25 -0400

    Time: 157 ms (66.10%), Space: 42.2 MB (56.70%) - LeetHub

--- 79-word-search/79-word-search.java
+++ 79-word-search/79-word-search.java
@@ -1,62 +1,56 @@
 class Solution {
-  
     char[][] matrix;
-    char[] letters;
-    boolean [][]visited;
+    char[] letter;
+    boolean[][] visited;
     public boolean exist(char[][] board, String word) {
-        int row=board.length;
+        int row= board.length;
         int col=board[0].length;
-        letters=word.toCharArray();
+        letter=word.toCharArray();
         matrix=board;
         for(int i=0;i<row;i++)
         {
             for(int j=0;j<col;j++)
             {
-                if(matrix[i][j]==letters[0])
+                if(matrix[i][j]==letter[0])
                 {
-                   visited =new boolean[row][col];
+                    visited= new boolean[row][col];
                     boolean res=dfs(i,j,0);
                     if(res==true)
                         return true;
+                    
                 }
+                
             }
         }
-       return false;
+        return false;
         
     }
-    
-    public boolean dfs(int row,int col, int index)
+    private boolean dfs(int row, int col, int index )
     {
-        if(index==letters.length)
+        //Base cases
+        if(index==letter.length)
             return true;
         if(row<0 || row>=matrix.length)
             return false;
-        if(col<0 || col>=matrix[0].length)
+         if(col<0 || col>=matrix[0].length)
             return false;
-        if(matrix[row][col]!=letters[index])
+        if(matrix[row][col]!=letter[index])
             return false;
-        if(visited[row][col] == true)
-            return false;
+        if(visited[row][col] == true) return false;
         
         visited[row][col]=true;
-        
         //Main Cases
+        boolean top=dfs(row-1, col, index+1);
+        boolean down=dfs(row+1, col, index+1);
+        boolean right=dfs(row, col+1, index+1);
+        boolean left=dfs(row, col-1, index+1);
         
-        boolean right = dfs(row,col+1,index+1);
-        boolean left = dfs(row,col-1,index+1);
-        boolean top = dfs(row-1,col,index+1);
-        boolean down = dfs(row+1,col,index+1);
-        
-        boolean ans= top || down || right || left;
-        if(ans!=true)
-        {
-            visited[row][col]=false;
+        boolean ans= top||down||left||right;
+        if(ans == false){
+           visited[row][col] = false;
         }
         return ans;
-        
-        
     }
 }
-
 
 
