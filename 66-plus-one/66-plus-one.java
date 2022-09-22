043fa7e1a1a2839b1e8c4c0ac04a65947b594016
Sanchit Bhargava <saanchit@gmail.com>
2022-09-22 03:39:52 -0400

    Time: 3 ms (5.17%), Space: 42.7 MB (25.13%) - LeetHub

---
+++ 66-plus-one/66-plus-one.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        int start=0;
+        int end=digits.length-1;
+        while(start<=end)
+        {
+            if(digits[end]+1!=10)
+            {
+                digits[end]=digits[end]+1;
+                return digits;
+            }
+            else
+            {
+                digits[end]=0;
+                System.out.print(end);
+                end--;
+            }
+        }
+        if(digits[0]==0)
+        {
+            int val[]= new int[digits.length+1];
+            val[0]=1;
+            return val;
+        }
+        return digits;
+    }
+}
