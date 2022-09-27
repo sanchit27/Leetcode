f2c7621635214d46c417fea4e3464be0496b6264
Sanchit Bhargava <saanchit@gmail.com>
2022-09-26 20:35:50 -0400

    Time: 2 ms (42.29%), Space: 43 MB (77.69%) - LeetHub

--- 78-subsets/78-subsets.java
+++ 78-subsets/78-subsets.java
@@ -7,14 +7,15 @@ class Solution {
     }
     public void findsubset(int nums[],int i,List<Integer> ans,List<List<Integer>> res)
     {
-        //If value is getting modified in all iteration, then always create a copy by using new ArrayList
-       // res.add(new ArrayList(ans));
+    
+      
         for(int j=i;j<nums.length;j++)
         {
             ans.add(nums[j]);
             findsubset(nums,j+1,ans,res);
             ans.remove(ans.size()-1);
         }
+        //If value is getting modified in all iteration, then always create a copy by using new ArrayList
         res.add(new ArrayList(ans));
     
     }
