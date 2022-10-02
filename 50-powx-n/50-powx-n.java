4f8eedfe5948a0cc1d2c54acedb17b5ca4a019d9
Sanchit Bhargava <saanchit@gmail.com>
2022-10-02 18:24:52 -0400

    Time: 4 ms (6.04%), Space: 42.2 MB (77.82%) - LeetHub

---
+++ 50-powx-n/50-powx-n.java
@@ -0,0 +1,32 @@
+class Solution {
+    public double myPow(double x, int n) {
+        if(n==0)
+            return 1;
+        if(n>0 && n%2==0)
+        {
+            double val=myPow(x,n/2);
+            return val*val;
+            
+        }
+        if(n>0 && n%2==1)
+        {
+            double val=myPow(x,n/2);
+            return val*val*x;
+            
+        }
+        if(n<0 && n%2==-1)
+        {
+            int pos= -n;
+            double val=myPow(x,(pos)/2);
+            System.out.println(val);
+            return 1/(x*val*val);    
+        }
+        if(n<0 && n%2==0)
+        {
+            int pos= -n;
+            double val=myPow(x,-(n/2));
+            return 1/(val*val);    
+        }
+        return 0;
+    }
+}
