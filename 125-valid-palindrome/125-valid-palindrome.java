667561998cf31801d1066f281e32d9706ee69105
Sanchit Bhargava <saanchit@gmail.com>
2022-10-03 01:01:35 -0400

    Time: 14 ms (47.39%), Space: 44.1 MB (47.88%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.java
+++ 125-valid-palindrome/125-valid-palindrome.java
@@ -1,7 +1,7 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        s = s.replaceAll(
-          "[^a-zA-Z0-9]", "");
+        //s = s.replaceAll(
+          //"[^a-zA-Z0-9]", "");
         s=s.toLowerCase();
 
         System.out.println(s.length());
@@ -9,18 +9,24 @@ class Solution {
         int i=0;
         if(s.length()==0||s.length()==1)
             return true;
-    
-        while(i!=j)
-        {if(i<=j)
+        while(i<j)
         {
+          
+        
+        while(i<j && !((s.charAt(i)>='a' && s.charAt(i)<='z') || (s.charAt(i)>='0'&&s.charAt(i)<='9'))){
+                i++;
+            }
+  
+            while(i<j && !((s.charAt(j)>='a' && s.charAt(j)<='z') || (s.charAt(j)>='0'&&s.charAt(j)<='9'))){
+                j--;
+            }
             if(s.charAt(i)!=s.charAt(j))
                 return false;
             i++;
             j--;
-        }else
-            return true;
-        }
+       
         
+    }
         
         return true;
         
