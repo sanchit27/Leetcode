f9a1e8287f8080c22bf82fcc59a3839e22ea4818
Sanchit Bhargava <saanchit@gmail.com>
2022-10-03 01:33:51 -0400

    Time: 12 ms (48.89%), Space: 43.5 MB (68.54%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.java
+++ 125-valid-palindrome/125-valid-palindrome.java
@@ -11,15 +11,19 @@ class Solution {
             return true;
         while(i<j)
         {
-          
+            while(i<j && !((s.charAt(i)>='a' && s.charAt(i)<='z') ||(s.charAt(i)>='0'&&s.charAt(i)<='9')))
+            { i++;}
+               //while(i<j && !((s.charAt(i)>='a' && s.charAt(i)<='z') || (s.charAt(i)>='0'&&s.charAt(i)<='9'))){
+              // i++;}
+            while(i<j && !((s.charAt(j)>='a'&& s.charAt(j)<='z')||(s.charAt(j)>='0'&& s.charAt(j)<='9')))
+         {j--;}
+           // while(i<j && !((s.charAt(j)>='a' && s.charAt(j)<='z') || (s.charAt(j)>='0'&&s.charAt(j)<='9'))){
+                //j--;
+            //}
         
-        while(i<j && !((s.charAt(i)>='a' && s.charAt(i)<='z') || (s.charAt(i)>='0'&&s.charAt(i)<='9'))){
-                i++;
-            }
+     
   
-            while(i<j && !((s.charAt(j)>='a' && s.charAt(j)<='z') || (s.charAt(j)>='0'&&s.charAt(j)<='9'))){
-                j--;
-            }
+            
             if(s.charAt(i)!=s.charAt(j))
                 return false;
             i++;
