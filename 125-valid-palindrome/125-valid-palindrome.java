6de74cd023ba0764d973625bd9880dd520df9ccf
Sanchit Bhargava <saanchit@gmail.com>
2022-10-03 01:34:06 -0400

    Time: 11 ms (49.91%), Space: 44 MB (52.66%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.java
+++ 125-valid-palindrome/125-valid-palindrome.java
@@ -13,16 +13,10 @@ class Solution {
         {
             while(i<j && !((s.charAt(i)>='a' && s.charAt(i)<='z') ||(s.charAt(i)>='0'&&s.charAt(i)<='9')))
             { i++;}
-               //while(i<j && !((s.charAt(i)>='a' && s.charAt(i)<='z') || (s.charAt(i)>='0'&&s.charAt(i)<='9'))){
-              // i++;}
+    
             while(i<j && !((s.charAt(j)>='a'&& s.charAt(j)<='z')||(s.charAt(j)>='0'&& s.charAt(j)<='9')))
          {j--;}
-           // while(i<j && !((s.charAt(j)>='a' && s.charAt(j)<='z') || (s.charAt(j)>='0'&&s.charAt(j)<='9'))){
-                //j--;
-            //}
-        
-     
-  
+          
             
             if(s.charAt(i)!=s.charAt(j))
                 return false;
