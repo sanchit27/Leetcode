2a257a3166f3890c2c547ebe95b6b25c08e6bd4d
Sanchit Bhargava <saanchit@gmail.com>
2022-10-04 15:56:51 -0400

    Time: 6 ms (56.19%), Space: 48.5 MB (43.90%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.java
+++ 47-permutations-ii/47-permutations-ii.java
@@ -12,7 +12,7 @@ private void backtrack(List<List<Integer>> list, List<Integer> tempList, int [] 
         list.add(new ArrayList<>(tempList));
     } else{
         for(int i = 0; i < nums.length; i++){
-            if(used[i] || i > 0 && nums[i] == nums[i-1] && !used[i - 1]) continue;
+            if(used[i] || i > 0 && nums[i] == nums[i-1] && used[i - 1]) continue;
             used[i] = true; 
             tempList.add(nums[i]);
             backtrack(list, tempList, nums, used);
