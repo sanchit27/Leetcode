c95d33f370987097596d6d25853c025d1e17a590
Sanchit Bhargava <saanchit@gmail.com>
2022-10-04 16:08:54 -0400

    Time: 4 ms (78.41%), Space: 47.3 MB (82.63%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.java
+++ 47-permutations-ii/47-permutations-ii.java
@@ -1,24 +1,30 @@
 class Solution {
     public List<List<Integer>> permuteUnique(int[] nums) {
-    
-    List<List<Integer>> list = new ArrayList<>();
-    Arrays.sort(nums);
-    backtrack(list, new ArrayList<>(), nums, new boolean[nums.length]);
-    return list;
-}
-
-private void backtrack(List<List<Integer>> list, List<Integer> tempList, int [] nums, boolean [] used){
-    if(tempList.size() == nums.length){
-        list.add(new ArrayList<>(tempList));
-    } else{
-        for(int i = 0; i < nums.length; i++){
-            if(used[i] || i > 0 && nums[i] == nums[i-1] && !used[i-1]) continue;
-            used[i] = true; 
-            tempList.add(nums[i]);
-            backtrack(list, tempList, nums, used);
-            used[i] = false; 
-            tempList.remove(tempList.size() - 1);
-        }
+        List<List<Integer>> res= new ArrayList<>();
+        List<Integer> ans= new ArrayList<>();
+        Arrays.sort(nums);
+        permutationstwo(res, ans,nums, new boolean[nums.length]);
+        return res;
     }
+    public void permutationstwo(List<List<Integer>> res,List<Integer> ans,int nums[], boolean vis[])
+    {
+        if(ans.size()==nums.length)
+        {
+        res.add(new ArrayList<>(ans));
+        }
+
+        for(int i=0;i<nums.length;i++)
+        {
+            if(vis[i])
+                continue;
+            if(vis[i]||i>0 && nums[i]==nums[i-1] && !vis[i-1])
+                continue;
+            vis[i]=true;
+            ans.add(nums[i]);
+            permutationstwo(res,ans,nums,vis);
+                vis[i]=false;
+            ans.remove(ans.size()-1);
+        
+        }
     }
 }
