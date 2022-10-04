430b87c45f32ff7cf8200ac835e890968f5f2978
Sanchit Bhargava <saanchit@gmail.com>
2022-10-04 15:51:50 -0400

    Time: 496 ms (7.97%), Space: 42.9 MB (95.53%) - LeetHub

---
+++ 47-permutations-ii/47-permutations-ii.java
@@ -0,0 +1,29 @@
+class Solution {
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer>> res= new ArrayList<>();
+        List<Integer> ans= new ArrayList<>();
+        permutationstwo(res, ans,nums, new boolean[nums.length]);
+        return res;
+    }
+    public void permutationstwo(List<List<Integer>> res,List<Integer> ans,int nums[], boolean vis[])
+    {
+        if(ans.size()==nums.length)
+        {if(!res.contains(ans))
+        res.add(new ArrayList<>(ans));
+        }
+
+        for(int i=0;i<nums.length;i++)
+        {
+            if(vis[i])
+                continue;
+            if(i>0 && nums[i]==nums[i-1] && vis[i-1])
+                continue;
+            vis[i]=true;
+            ans.add(nums[i]);
+            permutationstwo(res,ans,nums,vis);
+                vis[i]=false;
+            ans.remove(ans.size()-1);
+        
+        }
+    }
+}
