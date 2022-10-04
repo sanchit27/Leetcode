8b75c0bec57b039d67a5ca332bbb4dd45018654b
Sanchit Bhargava <saanchit@gmail.com>
2022-10-04 18:32:29 -0400

    Time: 43 ms (48.28%), Space: 54.4 MB (78.80%) - LeetHub

---
+++ 77-combinations/77-combinations.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> res= new ArrayList<>();
+        List<Integer> ans= new ArrayList<>();
+        combinations(1,n,k,ans,res);
+        return res;
+        
+    }
+public void combinations(int start, int n,int k,List<Integer> ans,List<List<Integer>> res)
+    {
+        if(ans.size()==k)
+            res.add(new ArrayList<>(ans));
+        for(int i=start;i<=n && ans.size()<k;i++)
+        {
+            ans.add(i);
+            combinations(i+1,n,k,ans,res);
+            ans.remove(ans.size()-1);
+        }
+    }
+}
