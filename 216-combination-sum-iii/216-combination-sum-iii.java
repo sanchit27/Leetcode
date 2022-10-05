b2a1283d9227c59ad463e0e4b4b1449d3b8830cc
Sanchit Bhargava <saanchit@gmail.com>
2022-10-04 23:07:04 -0400

    Time: 1 ms (86.92%), Space: 41.8 MB (41.25%) - LeetHub

--- 216-combination-sum-iii/216-combination-sum-iii.java
+++ 216-combination-sum-iii/216-combination-sum-iii.java
@@ -1,25 +1,25 @@
 class Solution {
-    List<List<Integer>> res= new ArrayList<>();
+    
     public List<List<Integer>> combinationSum3(int k, int n) {
-        
+        List<List<Integer>> res= new ArrayList<>();
         List<Integer> ans= new ArrayList<>();
-        combinationSumThree(n,k,1,ans,n);
+        combinationSumThree(n,k,1,ans,n,res);
         return res;
         
     }
-    public void combinationSumThree(int target,int k ,int index,List<Integer> list,int sum)
+    public void combinationSumThree(int n,int k ,int start,List<Integer> ans,int sum,List<List<Integer>> res)
     {
    
-          if(sum==0 && list.size()==k)
+          if(sum==0 && ans.size()==k)
           
-          {res.add(new ArrayList<>(list));}
+          {res.add(new ArrayList<>(ans));}
            
-        for(int i=index;i<10;i++)
+        for(int i=start;i<10;i++)
         {
            
-            list.add(i);
-            combinationSumThree(target, k, i + 1, list, sum - i);
-            list.remove(list.size()-1);
+            ans.add(i);
+            combinationSumThree(n, k, i + 1, ans, sum - i,res);
+            ans.remove(ans.size()-1);
             
         }
       
