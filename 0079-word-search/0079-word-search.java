c6f7ee3920f0b792c0d336ffd5183f692e0b3102
Sanchit Bhargava <saanchit@gmail.com>
2022-10-17 18:50:25 -0400

    Time: 144 ms (77.52%), Space: 42.5 MB (33.18%) - LeetHub

---
+++ 0079-word-search/0079-word-search.java
@@ -0,0 +1,58 @@
+class Solution {
+    char[][] matrix;
+    char[] letter;
+    boolean[][] visited;
+    public boolean exist(char[][] board, String word) {
+        int row= board.length;
+        int col=board[0].length;
+        letter=word.toCharArray();
+        matrix=board;
+        for(int i=0;i<row;i++)
+        {
+            for(int j=0;j<col;j++)
+            {
+                if(matrix[i][j]==letter[0])
+                {
+                    visited= new boolean[row][col];
+                    boolean res=dfs(i,j,0);
+                    if(res==true)
+                        return true;
+                    
+                }
+                
+            }
+        }
+        return false;
+        
+    }
+    private boolean dfs(int row, int col, int index )
+    {
+        //Base cases
+        if(index==letter.length)
+            return true;
+        if(row<0 || row>=matrix.length)
+            return false;
+         if(col<0 || col>=matrix[0].length)
+            return false;
+        if(matrix[row][col]!=letter[index])
+            return false;
+        if(visited[row][col] == true) return false;
+        
+        visited[row][col]=true;
+        //Main Cases
+        
+        int drow[]={-1,0,1,0};
+        int dcol[]={0,1,0,-1};
+        for(int i=0;i<drow.length;i++)
+        {
+       if(dfs(row-drow[i], col-dcol[i], index+1))
+           return true;
+        }
+        
+       // boolean ans= top||down||left||right;
+       // if(ans == false){
+           visited[row][col] = false;
+       // }
+        return false;
+    }
+}
