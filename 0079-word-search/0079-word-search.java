63349dc326e5918793ca682809bc912393ec8be7
Sanchit Bhargava <saanchit@gmail.com>
2022-10-17 19:40:15 -0400

    Time: 280 ms (20.76%), Space: 118.1 MB (10.15%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,47 +1,37 @@
 class Solution {
-     int drow[]={-1,0,1,0};
-        int dcol[]={0,1,0,-1};
     public boolean exist(char[][] board, String word) {
-        boolean[][] visited=new boolean[board.length][board[0].length];;
-    
+       boolean[][] visited = new boolean[board.length][board[0].length];
         for(int i=0;i<board.length;i++)
         {
-            for(int j=0;j<board[0].length;j++)
-            {
-                if(board[i][j]==word.charAt(0))
-                {
-                   if(dfs(i,j,0,word,board,visited))
-                        return true;
-                    
-                }
-                
-            }
+             for(int j=0;j<board[0].length;j++)
+                 if(board[i][j]==word.charAt(0))
+                 {
+                     
+                      if(fill(board,i,j,word,0,visited))
+                          return true;
+                 }
         }
         return false;
-        
-    }
-    private boolean dfs(int row, int col, int index, String word,char[][] board,boolean[][] visited)
-    {
-        //Base cases
-        if(index==word.length())
-            return true;
-        if(row<0 || row>=board.length||col<0 || col>=board[0].length||board[row][col]!=word.charAt(index)||visited[row][col])
-            return false;
-    
-        
-        visited[row][col]=true;
-        //Main Cases
-        
+     
        
+    }
+    public boolean fill(char[][] board,int sr,int sc, String word,int count,boolean[][] visited)
+    {
+        if(count==word.length())
+            return true;
+        if(sr<0||sc<0||sr>=board.length||sc>=board[sr].length||board[sr][sc]!=word.charAt(count)||visited[sr][sc])
+            return false;
+        
+        int drow[]={-1,0,1,0};
+        int dcol[]={0,1,0,-1};
+        visited[sr][sc]=true;
         for(int i=0;i<drow.length;i++)
         {
-       if(dfs(row-drow[i], col-dcol[i], index+1,word,board,visited))
-           return true;
+            
+            if(fill(board,sr-drow[i],sc-dcol[i],word,count+1,visited))
+                return true;    
         }
-        
-      
-           visited[row][col] = false;
-
+        visited[sr][sc]=false;
         return false;
     }
 }
