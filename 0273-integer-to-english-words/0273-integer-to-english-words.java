d1554d1d613d17a273f0e7570acb82d7dff02ab8
Sanchit Bhargava <saanchit@gmail.com>
2022-11-02 03:21:28 -0400

    Time: 16 ms (51.60%), Space: 42.6 MB (61.48%) - LeetHub

--- 0273-integer-to-english-words/0273-integer-to-english-words.java
+++ 0273-integer-to-english-words/0273-integer-to-english-words.java
@@ -32,7 +32,7 @@ class Solution {
     private String helper(int num) {
         if (num >= 1000000000) return (helper(num / 1000000000) + " Billion " + helper(num % 1000000000)).trim();
         if (num >= 1000000) return (helper(num / 1000000) + " Million " + helper(num % 1000000)).trim();
-        if (num >= 1000) return (helper(num / 1000) + " Thousand " + helper(num % 1000));
+        if (num >= 1000) return (helper(num / 1000) + " Thousand " + helper(num % 1000)).trim();
         if (num >= 100) return (helper(num / 100) + " Hundred " + helper((num % 100))).trim();
         if (num >= 20) return (tens[num / 10] + " " + helper(num % 10)).trim();
         if (num >= 1) return ones[num];
