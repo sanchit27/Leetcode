16f91cb4857ac9e66e4967b5e551c18ee985c2a0
Sanchit Bhargava <saanchit@gmail.com>
2022-11-02 03:21:18 -0400

    Time: 16 ms (51.60%), Space: 43.1 MB (27.41%) - LeetHub

--- 0273-integer-to-english-words/0273-integer-to-english-words.java
+++ 0273-integer-to-english-words/0273-integer-to-english-words.java
@@ -30,7 +30,7 @@ class Solution {
     }
 
     private String helper(int num) {
-        if (num >= 1000000000) return (helper(num / 1000000000) + " Billion " + helper(num % 1000000000));
+        if (num >= 1000000000) return (helper(num / 1000000000) + " Billion " + helper(num % 1000000000)).trim();
         if (num >= 1000000) return (helper(num / 1000000) + " Million " + helper(num % 1000000)).trim();
         if (num >= 1000) return (helper(num / 1000) + " Thousand " + helper(num % 1000));
         if (num >= 100) return (helper(num / 100) + " Hundred " + helper((num % 100))).trim();
