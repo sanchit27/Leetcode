b9c9626f999be419521617415ee49fb1d5035ee0
Sanchit Bhargava <saanchit@gmail.com>
2022-11-19 03:51:47 -0500

    Added solution - LeetHub

--- Merge Sort - GFG/merge-sort.java
+++ Merge Sort - GFG/merge-sort.java
@@ -59,52 +59,28 @@ class Solution
 {
     void merge(int arr[], int l, int m, int r)
     {
-         int n1 = m  -l+ 1;
-        int n2 = r - m;
- 
-        /* Create temp arrays */
-        int L[] = new int[n1];
-        int R[] = new int[n2];
- 
-        /*Copy data to temp arrays*/
-        for (int i = 0; i < n1; ++i)
-            L[i] = arr[l + i];
-        for (int j = 0; j < n2; ++j)
-            R[j] = arr[m + 1 + j];
- 
-        /* Merge the temp arrays */
- 
-        // Initial indexes of first and second subarrays
-        int i = 0, j = 0;
- 
-        // Initial index of merged subarray array
-        int k = l;
-        while (i < n1 && j < n2) {
-            if (L[i] <= R[j]) {
-                arr[k++] = L[i++];
-                //i++;
-            }
-            else {
-                arr[k++] = R[j++];
-                //j++;
-            }
-            //k++;
+        int merged[] = new int[r-l+1];
+        int idx1=l;
+        int idx2=m+1;
+        int x=0;
+        while(idx1<=m && idx2<=r)
+        {
+            if(arr[idx1]<=arr[idx2])
+            merged[x++]=arr[idx1++];
+            else
+            merged[x++]=arr[idx2++];
         }
- 
-        /* Copy remaining elements of L[] if any */
-        while (i < n1) {
-            arr[k] = L[i];
-            i++;
-            k++;
+        while(idx1<=m)
+        merged[x++]=arr[idx1++];
+        
+         while(idx2<=r)
+        merged[x++]=arr[idx2++];
+        int j=l;
+        for(int i=0;i<merged.length;i++)
+        {
+          
+            arr[j++]=merged[i];
         }
- 
-        /* Copy remaining elements of R[] if any */
-        while (j < n2) {
-            arr[k] = R[j];
-            j++;
-            k++;
-        }
-         
     }
     void mergeSort(int arr[], int l, int r)
     {
@@ -113,7 +89,7 @@ class Solution
         
           if (l < r) {
             // Find the middle point
-            int m = (l + r) / 2;
+            int m = l + (r - l) / 2;
  
             // Sort first and second halves
             mergeSort(arr, l, m);
